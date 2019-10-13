c0c5692f1a5e05afa147fff244e02548cc62fa37
HAB21 <Miguel>
2019-10-13 15:23:06 +0200

    examen1

--- dado_electronico.js
+++ dado_electronico.js
@@ -1,16 +1,25 @@
-let totalScore = 0;
+tirarDado = veces => {
+    let totalScore = 0;
 
-for(i = 0; i<=50; i++){
-    
-    let tirada = Math.ceil(Math.random() * 6);
-    console.log(`Nueva tirada: ${tirada}`)
-    
-    totalScore = totalScore + tirada;
-    console.log(`Llevas ${totalScore} puntos acumulados`);
+    for (i = 0; i <= veces; i++) {
 
-    if(totalScore >= 50){
-        console.log('¡¡PARTIDA TERMINADA!! Has realizado demasiadas tiradas con el dado y ya te has pasado de los 50 puntos')
-        break;
+        let tirada = Math.ceil(Math.random() * 6);
+        console.log(`Nueva tirada: ${tirada}`)
+
+        totalScore = totalScore + tirada;
+        console.log(`Llevas ${totalScore} puntos acumulados`);
+
+        if (totalScore === 50) {
+            console.log('¡¡PARTIDA TERMINADA!! Has realizado demasiadas tiradas con el dado y ya has llegado a los 50 puntos')
+            break;
+        }
+
+        else if (totalScore > 50) {
+            console.log('¡¡PARTIDA TERMINADA!! Has realizado demasiadas tiradas con el dado y ya te has pasado de los 50 puntos')
+            break;
+        }
     }
 }
+
+tirarDado(50);
 
